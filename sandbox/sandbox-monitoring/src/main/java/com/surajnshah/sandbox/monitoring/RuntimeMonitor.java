91e8a5218adcd86876aef6a0f4284c6ca5ce699c
surajshah <surajnshah@gmail.com>
2018-07-17 11:32:10 +0100

    add missing colon in output string to runtimemonitor

--- sandbox/sandbox-monitoring/src/main/java/com/surajnshah/sandbox/monitoring/RuntimeMonitor.java
+++ sandbox/sandbox-monitoring/src/main/java/com/surajnshah/sandbox/monitoring/RuntimeMonitor.java
@@ -19,7 +19,7 @@ public class RuntimeMonitor {
         /* This will return Long.MAX_VALUE if there is no preset limit */
         long maxMemory = Runtime.getRuntime().maxMemory();
         /* Maximum amount of memory the JVM will attempt to use */
-        System.out.println("Maximum memory (bytes) " + (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
+        System.out.println("Maximum memory (bytes): " + (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
 
         /* Total memory currently available to the JVM */
         System.out.println("Total memory available to JVM (bytes): " + Runtime.getRuntime().totalMemory());
