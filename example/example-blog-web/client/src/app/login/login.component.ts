7625643d8a47c13e55046dcb714e26e7a744a6dc
surajshah <surajnshah@gmail.com>
2018-07-22 15:59:09 +0100

    validate user login

--- example/example-blog-web/client/src/app/login/login.component.ts
+++ example/example-blog-web/client/src/app/login/login.component.ts
@@ -1,25 +1,32 @@
-import {Component} from '@angular/core';
-import {LoginService} from './login.service';
-import {User} from '../models/user.model';
+import { Component } from '@angular/core';
+import { LoginService } from './login.service';
+import { User } from '../models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  providers: [LoginService]
+  providers: [ LoginService ]
 })
 export class LoginComponent {
 
-  public user: User;
+  public user : User;
 
-  constructor(private loginService: LoginService) {
+  constructor(private loginService: LoginService, private router: Router) {
     this.user = new User();
   }
 
   validateLogin() {
-    if (this.user.username && this.user.password) {
+    if(this.user.username && this.user.password) {
       this.loginService.validateLogin(this.user).subscribe(result => {
         console.log('result is ', result);
+        if(result['status'] === 'success') {
+          this.router.navigate(['/home']);
+        } else {
+          alert('Wrong username password');
+        }
+
       }, error => {
         console.log('error is ', error);
       });
