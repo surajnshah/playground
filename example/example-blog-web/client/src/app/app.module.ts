09b36dd5d78ec45876043d3befd11160ba68877f
surajshah <surajnshah@gmail.com>
2018-07-22 16:08:33 +0100

    add home component and redirect from login

--- example/example-blog-web/client/src/app/app.module.ts
+++ example/example-blog-web/client/src/app/app.module.ts
@@ -6,11 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { RootComponent } from './root/root.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     RootComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
