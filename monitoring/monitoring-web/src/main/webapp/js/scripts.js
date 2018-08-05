d4f19e68e70c207d496405120577084b91f5815d
surajshah <surajnshah@gmail.com>
2018-08-05 16:48:03 +0100

    better javascript for monitoring-web

--- monitoring/monitoring-web/src/main/webapp/js/scripts.js
+++ monitoring/monitoring-web/src/main/webapp/js/scripts.js
@@ -28,58 +28,29 @@ request.onload = function() {
         table.setAttribute('class', 'table');
         const tbody = document.createElement('tbody');
 
-        const row1 = document.createElement('tr');
-        const th1 = document.createElement('th');
-        th1.setAttribute('scope', 'row');
-        th1.textContent = "Free Memory";
-        const td1 = document.createElement('td');
-        td1.textContent = data.freeMemory.toString();
+        var freeMemory = {element:"freeMemory", title:"Free Memory"};
+        var maxMemory = {element:"maxMemory", title:"Max Memory"};
+        var totalMemory = {element:"totalMemory", title:"Total Memory"};
+        var availableProcessors = {element:"availableProcessors", title:"Available Processors"};
+        var systemLoadAverage = {element:"systemLoadAverage", title:"System Load Average"};
 
-        const row2 = document.createElement('tr');
-        const th2 = document.createElement('th');
-        th2.setAttribute('scope', 'row');
-        th2.textContent = "Max Memory";
-        const td2 = document.createElement('td');
-        td2.textContent = data.maxMemory.toString();
-
-        const row3 = document.createElement('tr');
-        const th3 = document.createElement('th');
-        th3.setAttribute('scope', 'row');
-        th3.textContent = "Total Memory";
-        const td3 = document.createElement('td');
-        td3.textContent = data.totalMemory.toString();
-
-        const row4 = document.createElement('tr');
-        const th4 = document.createElement('th');
-        th4.setAttribute('scope', 'row');
-        th4.textContent = "Available Processors";
-        const td4 = document.createElement('td');
-        td4.textContent = data.availableProcessors.toString();
-
-        const row5 = document.createElement('tr');
-        const th5 = document.createElement('th');
-        th5.setAttribute('scope', 'row');
-        th5.textContent = "System Load Average";
-        const td5 = document.createElement('td');
-        td5.textContent = data.systemLoadAverage.toString();
+        var monitors = [freeMemory, maxMemory, totalMemory, availableProcessors, systemLoadAverage];
 
         div.appendChild(table);
         table.appendChild(tbody);
-        tbody.appendChild(row1);
-        row1.appendChild(th1);
-        row1.appendChild(td1);
-        tbody.appendChild(row2);
-        row2.appendChild(th2);
-        row2.appendChild(td2);
-        tbody.appendChild(row3);
-        row3.appendChild(th3);
-        row3.appendChild(td3);
-        tbody.appendChild(row4);
-        row4.appendChild(th4);
-        row4.appendChild(td4);
-        tbody.appendChild(row5);
-        row5.appendChild(th5);
-        row5.appendChild(td5);
+
+        //var i;
+        for (var i = 0; i < monitors.length; i++) {
+            const row = document.createElement('tr');
+            const th = document.createElement('th');
+            th.setAttribute('scope', 'row');
+            th.textContent = monitors[i].title;
+            const td = document.createElement('td');
+            td.textContent = data[monitors[i].element].toString();
+            tbody.appendChild(row);
+            row.appendChild(th);
+            row.appendChild(td);
+        }
 
     } else {
         console.log('Error...');
