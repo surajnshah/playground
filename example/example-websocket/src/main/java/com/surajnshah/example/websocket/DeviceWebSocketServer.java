dcd963af6721852cd2e7d5fb684baf5b74c9cae2
surajshah <surajnshah@gmail.com>
2018-08-10 23:43:02 +0100

    websocket example app

---
+++ example/example-websocket/src/main/java/com/surajnshah/example/websocket/DeviceWebSocketServer.java
@@ -0,0 +1,68 @@
+package com.surajnshah.example.websocket;
+
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import javax.enterprise.context.ApplicationScoped;
+import java.io.StringReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.surajnshah.example.model.Device;
+
+/**
+ * @author surajshah on 10/08/2018
+ * @project surajnshah.com
+ */
+
+@ApplicationScoped
+@ServerEndpoint("/actions")
+public class DeviceWebSocketServer {
+
+    @Inject
+    private DeviceSessionHandler sessionHandler;
+
+    @OnOpen
+    public void open(Session session) {
+        sessionHandler.addSession(session);
+    }
+
+    @OnClose
+    public void close(Session session) {
+        sessionHandler.removeSession(session);
+    }
+
+    @OnError
+    public void onError(Throwable error) {
+        Logger.getLogger(DeviceWebSocketServer.class.getName()).log(Level.SEVERE, null, error);
+    }
+
+    @OnMessage
+    public void handleMessage(String message, Session session) {
+        try (JsonReader reader = Json.createReader(new StringReader(message))) {
+            JsonObject jsonMessage = reader.readObject();
+
+            if ("add".equals(jsonMessage.getString("action"))) {
+                Device device = new Device();
+                device.setName(jsonMessage.getString("name"));
+                device.setDescription(jsonMessage.getString("description"));
+                device.setType(jsonMessage.getString("type"));
+                device.setStatus("Off");
+                sessionHandler.addDevice(device);
+            }
+
+            if ("remove".equals(jsonMessage.getString("action"))) {
+                int id = (int) jsonMessage.getInt("id");
+                sessionHandler.removeDevice(id);
+            }
+
+            if ("toggle".equals(jsonMessage.getString("action"))) {
+                int id = (int) jsonMessage.getInt("id");
+                sessionHandler.toggleDevice(id);
+            }
+        }
+    }
+}
